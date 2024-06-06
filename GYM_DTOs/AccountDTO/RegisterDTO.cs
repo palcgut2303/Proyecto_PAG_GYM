9131802e19852112ae81f3715b2cf04ddac4d9a9
palcgut2303 <146340194+palcgut2303@users.noreply.github.com>
2024-06-06 20:03:08 +0200

    cambios de comprobaciones de registro

--- GYM_DTOs/AccountDTO/RegisterDTO.cs
+++ GYM_DTOs/AccountDTO/RegisterDTO.cs
@@ -14,13 +14,16 @@ namespace GYM_DTOs.AccountDTO
         [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Obligatorio")]
+        [RegularExpression("^(?!\\d+$).*$", ErrorMessage = "El nombre de usuario no puede estar compuesto solo de números.")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Obligatorio")]
+        [RegularExpression("^[^0-9]*$", ErrorMessage = "El nombre completo no puede contener números.")]
         public string FullName { get; set; }
 
         [Required(ErrorMessage = "Obligatorio")]
         [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
         public string Password { get; set; }
 
         public string? EmailCode { get; set; }
