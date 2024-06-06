b38b62d7a0691b85dc02391d63baf92613928800
pablo.alcudia <pablo.alcudia@AM-COR-POR-005>
2024-06-06 13:03:28 +0200

    correccion comprobacion a la hora de editar que no sea la misma fecha que otra clase, ordenar todas las clases en las tablas por fecha

---
+++ GYM_BackendTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+using GYM_Backend.Repositories;
+using GYM_DTOs.AccountDTO;
+
+namespace GYM_BackendTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            ////Arrange
+            //string correo = "pabloalcudia18@gmail.com";
+            //string password = "12345678aA@";
+
+            //LoginDTO login = new LoginDTO
+            //{
+            //    Email = correo,
+            //    Password = password
+            //};
+
+            //Act
+            
+
+            //Assert
+
+
+           
+        }
+    }
+}
